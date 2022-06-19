b8e9588e0fbe0eae3e854b52264ef5e422f0d79f
Anvarov23 <anvarovjasurbek2003@gmail.com>
2022-06-19 17:16:29 +0500

    exam

---
+++ js/main.js
@@ -0,0 +1,72 @@
+// --------------------------------- Masala-1 ---------------------------------
+
+// var numbers=[2,3,0,6,0,5,7,0];
+// function addition(array){
+
+// var count = []
+
+// for(summ of array){
+//     if (summ == 0){
+//         count.push(summ)
+//     }else{
+//         count.unshift(summ)
+//     }
+// }
+// return count.join("") 
+// }
+
+// console.log(addition(numbers));
+
+// --------------------------------- Masala-2 ---------------------------------
+
+// var numbers=[true, false, true, false, true, true, true];
+
+// function one(array){
+
+//     var summ = 0
+//     for (num of array) {
+//         if(num > 0){
+//             summ++
+//         }
+//     }
+//     return summ;
+// }
+// console.log(one(numbers));
+
+// --------------------------------- Masala-3 ---------------------------------
+
+// var numbers = [-4, 7, -6, 8, 9, 115];
+
+// function jami(array){
+//     var summ = 0;
+
+//     for(arr of array){
+//         if(arr > 0){
+//             summ += arr
+//         }
+//     }
+//     return summ;
+// }
+// console.log(jami(numbers));
+
+// --------------------------------- Masala-4 ---------------------------------
+
+
+// var quantity = [
+//     {name:"Ali", price:200},
+//     {name:"Umid", price:800},
+//     {name:"Sardor", price:700}
+// ];  
+
+// function fun(array){
+//     var max = array[0].price;
+
+//     var summ = "";
+//     for(i of array){
+//         if(i.price > max){
+//             max = i.price;
+//             summ = i.name;
+//         }
+//     }return summ;
+// }
+// console.log(fun(quantity))
